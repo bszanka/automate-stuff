d5a01e84d705a8e7e12e1be868b0fba7c80d9c31
Balazs Szanka <szankabalazs@gmail.com>
2022-09-10 20:02:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import webbrowser, requests, bs4
+
+res = requests.get('https://hardverapro.hu/aprok/keres.php?stext=herman+miller&stcid_text=&stcid=&stmid_text=&stmid'
+                   '=&minprice=&maxprice=&cmpid_text=&cmpid=&usrid_text=&usrid=&__buying=0&__buying=1&stext_none=')
+
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+cities = soup.select('div.media-body > div.uad-info > div.uad-light')
+for i in cities:
+    print(i.text)
