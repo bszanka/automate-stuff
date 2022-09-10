bff257bcc792a785f0c4738991f24117c55fe880
Balazs Szanka <szankabalazs@gmail.com>
2022-09-10 20:59:38 +0200

    Remade the previous task in Selenium

--- main.py
+++ main.py
@@ -2,22 +2,33 @@ import requests, bs4
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromiumService
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 
 driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 
-driver.get('http://www.google.com/');
+driver.get('https://hardverapro.hu/index.html')
 
-time.sleep(3) # Let the user actually see something!
+searchBar = driver.find_element(By.CSS_SELECTOR, '#top > div.search-bar.search-bar-ha > div.search-bar-search > div > '
+                                                 'form > div.form-group.mb-0.d-flex.justify-content-center.flex-wrap '
+                                                 '> div > input')
+searchBar.send_keys('herman miller')
+searchBar.submit()
+
+cities = driver.find_elements(By.CSS_SELECTOR, 'div.media-body > div.uad-info > div.uad-light')
+for i in cities:
+    print(i.text)
+
+time.sleep(10)  # Let the user actually see something!
 
 driver.quit()
 
-res = requests.get('https://hardverapro.hu/aprok/keres.php?stext=herman+miller&stcid_text=&stcid=&stmid_text=&stmid'
-                   '=&minprice=&maxprice=&cmpid_text=&cmpid=&usrid_text=&usrid=&__buying=0&__buying=1&stext_none=')
-
-soup = bs4.BeautifulSoup(res.text, 'html.parser')
-
-cities = soup.select('div.media-body > div.uad-info > div.uad-light')
-for i in cities:
-    print(i.text)
+# res = requests.get('https://hardverapro.hu/aprok/keres.php?stext=herman+miller&stcid_text=&stcid=&stmid_text=&stmid'
+#                    '=&minprice=&maxprice=&cmpid_text=&cmpid=&usrid_text=&usrid=&__buying=0&__buying=1&stext_none=')
+#
+# soup = bs4.BeautifulSoup(res.text, 'html.parser')
+#
+# cities = soup.select('div.media-body > div.uad-info > div.uad-light')
+# for i in cities:
+#     print(i.text)
